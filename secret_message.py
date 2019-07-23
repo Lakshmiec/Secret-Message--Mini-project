a746fbe22e23589204ee53ddb98747baa1946f0c
Sreelakshmi E C <lakshmiec98@gmail.com>
2019-07-23 11:52:58 +0530

    Update secret_message.py

--- secret_message.py
+++ secret_message.py
@@ -1,10 +1,10 @@
 import os
 def rena_files():
-     file_list= os.listdir(r"C:\Users\3 Kutties\OneDrive\Documents\prank")
+     file_list= os.listdir(r"C:\Users\3 Kutties\OneDrive\Documents\prank")  #returns a list containing the names of the entries in the directory given by path
      print(file_list)
-     print("The current"+os.getcwd())
-     os.chdir(r"C:\Users\3 Kutties\OneDrive\Documents\prank")
+     print("The current working directory"+os.getcwd())             # to get current working directory
+     os.chdir(r"C:\Users\3 Kutties\OneDrive\Documents\prank")        #to change the current working directory
      for file_name in file_list:
           file_table=str.maketrans("0123456789", "          ","0123456789")
-          os.rename(file_name,file_name.translate(file_table))
+          os.rename(file_name,file_name.translate(file_table))    # for renaming the files
 rena_files()
